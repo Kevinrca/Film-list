14eac36199e9191c16d2159c7084ebf04692c46c
Kévin Rocha <neonix75@gmail.com>
2020-07-26 23:37:35 +0200

    Mise en place des foncionnalités de base (ajout, suppression et et bouton vu)

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 let movieList = document.querySelector(".movieList");
 let movieListSeen = document.querySelector(".movieListSeen");
 
-
+let movieCount = 1;
 
 
 
 function addMovie () {
     const movieTitle = document.querySelector(".movieTitle").value;
 
-    if(movieTitle == "") {
+    if(movieTitle == "") {    //Vérifier qu'un texte à bien été écrit
         alert("Indiquez le titre de votre film.");
     }
-    else {        // Ajoute le film à la liste avec un bouton supprimer et un bouton vu 
-        movieList.innerHTML += `<li>${movieTitle} <button id="${movieTitle}" onclick=suppMovie(this.id)>Supprimer</button><button id="${movieTitle}" onclick=addToSeenMovie(this.id)>Vu</button></li>`;
+    else { 
+        
+        movieList.innerHTML += `<li id=${movieCount}>${movieCount} - ${movieTitle} <button id="${movieTitle}" onclick=suppMovie(this.id)>Supprimer</button><button id="${movieTitle}" onclick=addToSeenMovie(this.id)>Vu</button></li>`; // ajouter un film avec un bouton suppr et un bouton vu
+
+        movieCount++;
+
+        document.querySelector(".movieTitle").value = "";   // supprimer le texte dans le input
+    }
+
+    if(movieList !== "") {
+        document.querySelector(".movieListNoneText").style.display = "none";   // Enlever le texte "pas encore de film ajouté"
     }
 }
-
-
-
 
 
 
@@ -28,6 +34,8 @@ function suppMovie (id) {
 }
 
 function addToSeenMovie (id) {
+    document.querySelector(".movieListSeenNoneText").style.display = "none";
+
     movieListSeen.innerHTML += `<li>${id} <button id="${id}" onclick=suppMovie(this.id)>Supprimer</button>`;
     suppMovie(id);
 }
