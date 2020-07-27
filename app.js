6d237c1be8a5253869c0c0af26f1d21e1a2362f1
Kévin Rocha <neonix75@gmail.com>
2020-07-27 10:57:23 +0200

    Relier la liste HTML avec un tableau JavaScript

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 let movieList = document.querySelector(".movieList");
 let movieListSeen = document.querySelector(".movieListSeen");
 
-let movieCount = 1;
+let movieListArray = [];
 
 
 
 function addMovie () {
     const movieTitle = document.querySelector(".movieTitle").value;
-
-    if(movieTitle == "") {    //Vérifier qu'un texte à bien été écrit
+    
+    //Vérifier qu'un texte à bien été écrit
+    if(movieTitle == "") {    
         alert("Indiquez le titre de votre film.");
     }
     else { 
+        // Mettre de film dans l'array
+        movieListArray.push(movieTitle); 
         
-        movieList.innerHTML += `<li id=${movieCount}>${movieCount} - ${movieTitle} <button id="${movieTitle}" onclick=suppMovie(this.id)>Supprimer</button><button id="${movieTitle}" onclick=addToSeenMovie(this.id)>Vu</button></li>`; // ajouter un film avec un bouton suppr et un bouton vu
+        // ajouter un film avec un bouton suppr et un bouton vu
+        movieList.innerHTML += `<li>${movieListArray[movieListArray.indexOf(movieTitle)]}
+        <button id="${movieListArray[movieListArray.indexOf(movieTitle)]}" onclick=suppMovie(this.id)>Supprimer</button>
+        <button id="${movieListArray[movieListArray.indexOf(movieTitle)]}" onclick=addToSeenMovie(this.id)>Vu</button></li>`; 
+        
 
-        movieCount++;
-
-        document.querySelector(".movieTitle").value = "";   // supprimer le texte dans le input
+        // supprimer le texte dans le input
+        document.querySelector(".movieTitle").value = "";   
     }
 
+
     if(movieList !== "") {
-        document.querySelector(".movieListNoneText").style.display = "none";   // Enlever le texte "pas encore de film ajouté"
+        
+        // Enlever le texte "pas encore de film ajouté"
+        document.querySelector(".movieListNoneText").style.display = "none";   
     }
 }
 
@@ -30,8 +39,17 @@ function addMovie () {
 
 
 function suppMovie (id) {
+    // Prendre l'index du film à supprimer
+    let index = movieListArray.indexOf(id);
+    
+    // Supprimer le film de la liste HTML
     document.getElementById(id).parentElement.outerHTML = "";
+
+    // supprimer le film de l'array
+    movieListArray.splice(index, 1);
 }
+
+
 
 function addToSeenMovie (id) {
     document.querySelector(".movieListSeenNoneText").style.display = "none";
